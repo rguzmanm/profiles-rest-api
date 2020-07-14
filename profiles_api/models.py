115549650760f0dfc4b27847f1a8a52a4c5441de
rguzmanm <rguzmanm15@gmail.com>
2020-07-14 17:24:02 -0500

    Correction to the ccreate user class

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -16,7 +16,7 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name)
 
         user.set_password(password)
-        user.save(using.self_db)
+        user.save(using=self._db)
 
         return user
 
