e22e0c29ad7268b6aa16e4ad2efca9b019c83a5d
rguzmanm <rguzmanm15@gmail.com>
2020-07-25 12:19:49 -0500

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,11 +3,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.settings import api_settings
+from rest_framework.authtoken.views import ObtainAuthToken
+
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
-from rest_framework.authentication import TokenAuthentication
-from rest_framework import filters
 
 
 class HelloApiView(APIView):
@@ -83,7 +86,7 @@ class HelloViewSet(viewsets.ViewSet):
         return Response({'http_method': 'GET'})
 
     def update(self, request, pk=None):
-        # Handle updating an object 
+        # Handle updating an object
         return Response({'http_method': 'PUT'})
 
     def partial_update(self, request, pk=None):
@@ -103,3 +106,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email')
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
