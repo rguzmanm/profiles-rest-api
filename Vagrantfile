1616f0f39f02d97ad7da80fe89a43e10a049b096
rguzmanm <rguzmanm15@gmail.com>
2020-07-25 11:56:23 -0500

    Added profiles viewset

--- Vagrantfile
+++ Vagrantfile
@@ -29,4 +29,8 @@ Vagrant.configure("2") do |config|
       echo "alias python='python3'" >> /home/vagrant/.bash_aliases
     fi
   SHELL
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.name = "django profiles-api"
+  end
  end
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+from rest_framework.authentication import TokenAuthentication
 
 
 class HelloApiView(APIView):
@@ -89,3 +92,12 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         # Handle removing an object
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    # Handle createing and updating profiles
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
